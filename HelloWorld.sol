8bf9f960059664b9cd5c86196ae90d56c117dd23
if164ever1 <zadorozhniak.ihor@gmail.com>
2025-10-13 19:54:47 -0600

    hello world

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,9 +1,20 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >= 0.7.0 <0.9.0;
 
 contract HelloWorld{
-    constructor(){}
+    string private text;
 
-    function helloworld() public view returns (string memory){}
+    constructor(){
+        text = "Hello world";
+    }
+
+    function helloworld() public view returns (string memory){
+        return text;
+    }
+
+    function setText(string memory newText) public {
+        text = newText;
+    }
 }
 
 
