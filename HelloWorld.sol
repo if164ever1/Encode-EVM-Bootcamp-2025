d2864186bc39d0d2975f572ea43aec4fae83776a
if164ever1 <zadorozhniak.ihor@gmail.com>
2025-10-19 09:49:39 -0600

    functions getters and setters

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -5,7 +5,7 @@ contract HelloWorld{
     string private text;
 
     constructor(){
-        text = "Hello world";
+        text = initialText();
     }
 
     function helloworld() public view returns (string memory){
@@ -15,7 +15,36 @@ contract HelloWorld{
     function setText(string memory newText) public {
         text = newText;
     }
+
+    function initialText() internal pure returns(string memory) {
+        return "Hello world";
+    }
+
+    function _isUnchanged() internal view returns (bool) {
+        return keccak256(bytes(text)) == keccak256(bytes(initialText()));
+    }
+
+    function textHasChanged() public view returns (bool returnValue_) {
+        returnValue_ = !_isUnchanged();
+    }
+
+    function _restore() internal {
+        text = initialText();
+    }
+
+    function restore() public returns (bool){
+        if (_isUnchanged()) return false;
+        _restore();
+        return true;
+    }
+}
+
+contract InheritedHelloWorld is HelloWorld {
+    constructor() { 
+        setText("newText");
+    }
 }
 
 
 
+ 
